6e3b0c15d8cea18fc5ebce9aeffa46e9fbc7bfd8
Sylvino Prevot <sylvino.junior@calindra.com.br>
2024-10-23 17:58:40 -0300

    mudando user_seeder

--- database/seeders/user_seeder.ts
+++ database/seeders/user_seeder.ts
@@ -1,14 +1,12 @@
-import Hash from '@adonisjs/core/services/hash'
 import User from '#models/user'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
 export default class UserSeeder extends BaseSeeder {
   public async run() {
-    // Criar um novo usuário
     await User.create({
       fullName: 'John Doe',
-      email: 'doe@gmail.com',
-      password: await Hash.make('password123'), // Certifique-se de hashear a senha
+      email: 'john@gmail.com',
+      password: 'alo123',
     })
   }
 }
