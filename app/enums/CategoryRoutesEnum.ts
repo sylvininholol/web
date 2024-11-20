3df667950f59cd2fe0972891b58c13bf2a8333e7
Sylvino Prevot <sylvino.junior@calindra.com.br>
2024-11-20 04:58:52 -0300

    Criando enum de Categorias para rotas

---
+++ app/enums/CategoryRoutesEnum.ts
@@ -0,0 +1,11 @@
+export enum CategoryRoutes {
+    CAMISETAS = 'CAMISETA',
+    REGATAS = 'REGATA',
+    CALCAS = 'CALCA',
+    UNDERWEAR = 'UNDERWEAR',
+    SHORTS = 'SHORT',
+    MEIA = 'MEIA',
+    CASACOS = 'CASACO',
+    TENIS = 'TENIS',
+    BONES = 'BONES',
+  }
